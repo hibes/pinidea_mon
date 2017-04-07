3000861dd47e2e83aa18d9f21a00dad21ef6c6f8
Henry Brow <hankbrown@gmail.com>
2017-04-07 09:07:36 +0100

    Added an interactive start script (puts you in a bash shell).

---
+++ scripts/dockerStartInteractive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Get directory path of *this* script file and exit if is not set, NULL, or an empty string
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+SCRIPTS_DIR="${SCRIPTS_DIR:?}"
+
+. ${SCRIPTS_DIR}/shared.lib.sh
+
+if ! docker images | grep "${DOCKER_IMAGE_NAME}"; then
+  ${SCRIPTS_DIR}/build.sh
+fi
+
+${SUDO} docker run --rm -ti "${DOCKER_IMAGE_NAME}" /bin/sh
